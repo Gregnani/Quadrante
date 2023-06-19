5e319709156d75d421cb57f728c58af471dd0c87
gregnani <fgregnani@gmail.com>
2023-06-18 21:58:52 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+
+double cordenadaX;
+double cordenadaY;
+string local;
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine("---Quadrante Carteziano---\n");
+
+Console.ResetColor();
+
+Console.WriteLine("Entre com o ponto desejado\n");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.Write("Cordenada X:");
+cordenadaX = Convert.ToDouble(Console.ReadLine())!;
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+Console.Write("Cordenada Y:");
+cordenadaY = Convert.ToDouble(Console.ReadLine())!;
+
+Console.ResetColor();
+
+if(cordenadaX == 0 && cordenadaY == 0 ) local = "a origem";
+else if (cordenadaX == 0) local = "o eixo das ordenadas";
+else if (cordenadaY == 0) local = "o eixo das abscissas";
+ 
+     {
+    if (cordenadaX > 0)
+        local = "quadrante 1";
+    else
+        local = "quadrante 2";
+    }
+
+    {
+    if (cordenadaX < 0)
+        local = "quadrante 3";
+    else
+        local = "quadrante 4";
+    }
+
+    Console.WriteLine($"\nO ponto ({cordenadaX}, {cordenadaY}) fica no {local}.\n");
+
